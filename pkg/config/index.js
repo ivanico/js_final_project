5aceba434e3c83728472280affc5cd014c28ad55
ivanico <55762391+ivanico@users.noreply.github.com>
2020-09-14 16:15:41 +0200

    Update index.js

--- pkg/config/index.js
+++ pkg/config/index.js
@@ -3,13 +3,13 @@ const fs = require('fs');
 const confFile = './config.json';
 var config = null;
 
-const get = (pero) => {
+const get = (part) => {
     if(config === null) {
         let data = fs.readFileSync(confFile, 'utf8');
         config = JSON.parse(data);
     }
-    if(config[pero]) {
-        return config[pero];
+    if(config[part]) {
+        return config[part];
     }
     return null;
 };
